206eb92f3d459291191e647c08320c2622862b68
Cole M. Bonnema <32077829+NervesOfJello@users.noreply.github.com>
2018-03-26 01:21:41 -0500

    Making Menus Look nice

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -1,18 +1,49 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Menu : MonoBehaviour {
+
+    private bool hasSeenCredits = false;
 
 	// Use this for initialization
 	void Start () 
 	{
-		
+        DontDestroyOnLoad(gameObject);
 	}
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		
-	}
+    public void ToCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+
+    public void ToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void onClickPlay()
+    {
+        if (hasSeenCredits)
+        {
+            SceneManager.LoadScene("ArenaLevel");
+            Destroy(gameObject, 1);
+        }
+        else
+        {
+            hasSeenCredits = true;
+            SceneManager.LoadScene("Instructions");
+        }
+    }
+
+    //quit if it's a build, stop if it's in the editor
+    public void OnClickExit()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
 }
